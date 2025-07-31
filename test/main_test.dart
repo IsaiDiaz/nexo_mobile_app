1b761d22e9aa25a40eae6ab822b75a7eb3ae9c55
IsaiDiaz <jsoza057@gmail.com>
2025-07-30 21:46:17 -0400

    test basico creado

---
+++ test/main_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Test básico', () {
+    test('1 + 1 debería ser igual a 2', () {
+      expect(1 + 1, equals(2));
+    });
+  });
+}
